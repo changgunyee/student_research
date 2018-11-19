141a4ac9f3e7d6cdf72c6807f70d55f38321ef2d
changgunyee <42175543+changgunyee@users.noreply.github.com>
2018-11-19 16:00:39 +0900

    ss

--- myproject_django/polls/urls.py
+++ myproject_django/polls/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from django.contrib import admin
 from . import views
 
+app_name='polls'
 urlpatterns = [
     path('', views.home, name='home'),
     path('upload',views.upload,name='upload'),
+    path('person/<int:person_id>',views.answers_by_person,name='answers_by_person'),
 ]
 
--- myproject_django/polls/views.py
+++ myproject_django/polls/views.py
@@ -2,8 +2,35 @@ from django.urls import path
 from django.contrib import admin
 from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('upload',views.upload,name='upload'),
-]
+def upload(request):
+    question_excel_file = request.FILES.get("question_excel_file",False)
+    if question_excel_file:
+        question_df=pd.read_excel(question_excel_file,sheet_name=0)
+        question_array=question_df.values
+        question_manager=Question.objects
+        classification_manager=Classification.objects
+        for arr in question_array:
+            question_manager.create_by_arr(arr)
+
+    answer_excel_file=request.FILES.get("answer_excel_file",False)
+    if answer_excel_file:
+        answer_df=pd.read_excel(answer_excel_file,sheet_name=0)
+        columns=answer_df.columns.tolist()
+        for index in range(2,len(columns)):
+            columns[index]=re.split('[\D]+',columns[index])
+            question=question_manager.get(number=columns[index][2])
+            columns[index]=question
+
+
+        for index_row, row in answer_df.iterrows():
+            person=Person.objects.create(name=row[0],email=row[1])
+            for index in range(2,len(row)):
+                row[index]=re.sub('[\W]+','',row[index])
+                answer=Answer.objects.get(question=columns[index],answer=row[index])
+                person.answers.add(answer)
+            person.save()
+    return 
+
+#def answers_by_person(request,person_id):
+
 
