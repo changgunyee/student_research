fdc92ed923e4c20ab9efb7a27bd0c9e6d3f898d8
changgunyee <42175543+changgunyee@users.noreply.github.com>
2018-11-19 17:45:08 +0900

    d

--- myproject_nuxt/pages/index.vue
+++ myproject_nuxt/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <form action="54.180.115.81:8000/polls/upload" method="post" enctype="multipart/form-data">
-  
+  <form action="0.0.0.0:8000/polls/upload" method="post" enctype="multipart/form-data">
+
     <input type="file" title="Upload question excel file" name="question_excel_file" required="required">
 
     <input type="file" title="Upload answer excel file" name="answer_excel_file" required="required">
