fe3994c00aba30710245cd5748e6331c797e0aa4
changgunyee <42175543+changgunyee@users.noreply.github.com>
2018-11-19 19:29:03 +0900

    Update index.vue

--- myproject_nuxt/pages/index.vue
+++ myproject_nuxt/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <form action="http://54.180.115.81:8000/polls/upload" method="post" enctype="multipart/form-data">
-
+  {% csrf_token %}
     <input type="file" title="Upload question excel file" name="question_excel_file" required="required">
-
+    
     <input type="file" title="Upload answer excel file" name="answer_excel_file" required="required">
 
     <input type="submit" value="Upload" style="border: 1px solid green; padding:5px; border-radius: 2px; cursor: pointer;">
