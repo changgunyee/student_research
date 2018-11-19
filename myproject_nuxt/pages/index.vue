f7c7f704f9b8ce27781ec94937b403b70f829df4
changgunyee <42175543+changgunyee@users.noreply.github.com>
2018-11-20 01:27:29 +0900

    Update index.vue

--- myproject_nuxt/pages/index.vue
+++ myproject_nuxt/pages/index.vue
@@ -55,7 +55,7 @@ export default{
               'content-type': 'multipart/form-data'
             }
         }
-      return axios.post('http://127.0.0.1:8000/polls/upload',data,config).then((e)=>{
+      return axios.post('https://127.0.0.1:8000/polls/upload',data,config).then((e)=>{
           console.log(e)
       })
     }
