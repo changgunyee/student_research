0c74e9b3dd3ceb7a4c3cd06fceda50fa72239298
changgunyee <42175543+changgunyee@users.noreply.github.com>
2018-11-21 16:42:56 +0900

    Update settings.py

--- myproject_django/myproject_django/settings.py
+++ myproject_django/myproject_django/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = [
-    'http://54.180.115.81:80',
+    '54.180.115.81'
 ]
 
 WSGI_APPLICATION = 'myproject_django.wsgi.application'
