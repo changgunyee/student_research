209090c9eae8833a55467da2e67ef0107648975f
changgunyee <42175543+changgunyee@users.noreply.github.com>
2018-11-21 17:30:21 +0900

    Update views.py

--- myproject_django/polls/views.py
+++ myproject_django/polls/views.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from .models import *
+import json
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
