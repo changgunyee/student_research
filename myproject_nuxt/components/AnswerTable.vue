59e9deed23bac0a7e67e79cc2c11afef19bc585a
changgunyee <42175543+changgunyee@users.noreply.github.com>
2018-12-02 11:15:28 +0900

    Update AnswerTable.vue

--- myproject_nuxt/components/AnswerTable.vue
+++ myproject_nuxt/components/AnswerTable.vue
@@ -33,9 +33,9 @@ export default{
       getPersonData(){
         const config ={
             'content-type':'application/json',
-            headers:{
-            'Access-Control-Request-Headers': '',
-            },
+        //    headers:{
+         //   'Access-Control-Request-Headers': '',
+         //   },
         }
         return axios.get('http://54.180.115.81:8000/polls/person/0',config).then((response)=>{
             this.persons=response.data['persons']
