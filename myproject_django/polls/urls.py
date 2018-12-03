d4a83adbbe814a35a7ab5a66c30555181ac789cf
changgunyee <42175543+changgunyee@users.noreply.github.com>
2018-12-03 21:56:33 +0900

    Update urls.py

--- myproject_django/polls/urls.py
+++ myproject_django/polls/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name='polls'
 urlpatterns = [
     path('upload',views.upload),
-    path('person/page/<int:current_page>-<int:num>',views.person_by_page),
+    path('person/page/page<int:current_page>num<int:num>',views.person_by_page),
     path('answer_rate',views.answer_rate),
 ]
